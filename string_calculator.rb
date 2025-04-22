955fe08500de13b61b69206c69d5d07cfe141d58
Prashant <prashant9981sharma@gmail.com>
2025-04-22 23:12:13 +0530

    error handling

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,9 +4,18 @@ class StringCalculator
     return string.to_i if string.match?(/^\d+$/)
     numbers = string.gsub(/\\n/, ',').split(',').map(&:to_i)
     sum = 0
+    errors = []
     numbers.each do |number|
+      if number < 0
+        errors << number
+        next
+      end
       sum += number
     end
-    sum
+    if errors.any?
+      raise "Negative numbers not allowed: #{errors.join(', ')}"
+    else
+      sum
+    end
   end
 end
