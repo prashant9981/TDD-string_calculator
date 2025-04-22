acab59e1d5cb0b0c2b13a9527f356e23b2f4f016
Prashant <prashant9981sharma@gmail.com>
2025-04-22 21:25:04 +0530

    addition for string with numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,14 @@
 class StringCalculator
   def add(string)
     return 0 if string.empty?
+    return string.to_i if string.match?(/^\d+$/)
+    numbers = string.split(',')
+    sum = 0
+    numbers.each do |number|
+      if number.match?(/^\d+$/)
+        sum += number.to_i
+      end
+    end
+    sum
   end
 end
