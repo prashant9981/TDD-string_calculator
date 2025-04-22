e3fb4f9902a9e901632269d880ae1d5822f52b16
Prashant <prashant9981sharma@gmail.com>
2025-04-22 21:58:07 +0530

    \n delimeter handling

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe StringCalculator do
 
     it 'handles multiple numbers with new line delimeter' do
       calc = StringCalculator.new
-      expect(calc.add("1,2\n3")).to eq(6)
+      expect(calc.add('1,2\n3')).to eq(6)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,12 +2,10 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
     return string.to_i if string.match?(/^\d+$/)
-    numbers = string.split(',')
+    numbers = string.gsub(/\\n/, ',').split(',').map(&:to_i)
     sum = 0
     numbers.each do |number|
-      if number.match?(/^\d+$/)
-        sum += number.to_i
-      end
+      sum += number
     end
     sum
   end
